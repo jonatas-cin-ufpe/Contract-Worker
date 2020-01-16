b2e758b8fb069125de9d16c17533eae2d52c55fd
Jonatas Vitalino <jcv@cin.ufpe.br>
2020-01-16 20:11:08 -0300

    Composições e Classes Feitas de Verdade

--- Entities/Department.cs
+++ Entities/Department.cs
@@ -1,10 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+
 
 namespace Course.Entities
 {
     class Department
     {
+        public string Name { get; set; }
+
+        public Department()
+        {
+
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
     }
+
+
 }
